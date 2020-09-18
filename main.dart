f9404fce38a112e1a63ca25d2b876f231900a656
Sibi Seetha <71497602+SibiSeetha@users.noreply.github.com>
2020-09-18 16:35:06 +0530

    Flutter_Demo_App_From_Scratch

---
+++ main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+// This code is uploaded on my youtube channel: Sibi Seetha
+// https://www.youtube.com/channel/UCneNi9LE9JVhDcfeK42okJw/
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int counter = 0;
+  void increaseCounter() {
+    setState(() {
+      counter = counter + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter Demo Home Page"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "You have pushed the button so many times:",
+              ),
+              Text(
+                "$counter",
+                style: TextStyle(fontSize: 50),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: increaseCounter,
+        ),
+      ),
+    );
+  }
+}
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Center(
+//         child: Text(
+//           "Hello World!",
+//         ),
+//       ),
+//     );
+//   }
+// }
